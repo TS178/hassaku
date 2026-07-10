dd4ed59d869abde93d87e963fdbd8a348d4df74f
TS178 <mapplec1206@yahoo.co.jp>
2026-07-10 22:39:07 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -30,19 +30,19 @@ const CONFIG = {
    *    name… 表示名（自由に変更可）
    *    color… 地図アイコンの色（好みで変更可） */
   MIKOSHI: [
-    { id: "m01", name: "森之内",   color: "#e6194B" },
-    { id: "m02", name: "領家町",   color: "#3cb44b" },
-    { id: "m03", name: "里本江",   color: "#4363d8" },
-    { id: "m04", name: "地頭町",   color: "#f58231" },
-    { id: "m05", name: "高田",   color: "#911eb4" },
-    { id: "m06", name: "東小室",   color: "#008080" },
-    { id: "m07", name: "給分",   color: "#e6194B" },
-    { id: "m08", name: "和田",   color: "#3cb44b" },
-    { id: "m09", name: "七海",   color: "#4363d8" },
-    { id: "m10", name: "田中",   color: "#f58231" },
-    { id: "m11", name: "貝田", color: "#911eb4" },
-    { id: "m12", name: "大西", color: "#008080" },
-    { id: "m13", name: "相神", color: "#9A6324" },
-    { id: "m14", name: "中浜", color: "#000075" }
+    { id: "m01", name: "森之内", color: "#c0392b", icon: "img/m01.png" },
+    { id: "m02", name: "領家町",   color: "#e67e22", icon: "img/m02.png" },
+    { id: "m03", name: "里本江", color: "#f1c40f", icon: "img/m03.png" },
+    { id: "m04", name: "地頭町",   color: "#27ae60", icon: "img/m04.png" },
+    { id: "m05", name: "高田",     color: "#16a085", icon: "img/m05.png" },
+    { id: "m06", name: "東小室",   color: "#2980b9", icon: "img/m06.png" },
+    { id: "m07", name: "給分",   color: "#2c3e90", icon: "img/m07.png" },
+    { id: "m08", name: "和田",     color: "#8e44ad", icon: "img/m08.png" },
+    { id: "m09", name: "七海",     color: "#c0399b", icon: "img/m09.png" },
+    { id: "m10", name: "田中",     color: "#7f8c1a", icon: "img/m10.png" },
+    { id: "m11", name: "貝田",     color: "#9A6324", icon: "img/m11.png" },
+    { id: "m12", name: "大西",     color: "#111111", icon: "img/m12.png" },
+    { id: "m13", name: "相神",   color: "#d81b60", icon: "img/m13.png" },
+    { id: "m14", name: "中浜",   color: "#e84393", icon: "img/m14.png" }
   ]
 };
