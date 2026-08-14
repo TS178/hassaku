1e2a04dbfb27ad7b53bbd39d2af4aefb7db73cd1
TS178 <mapplec1206@yahoo.co.jp>
2026-08-14 20:34:49 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -43,7 +43,8 @@ const CONFIG = {
     { id: "m11", name: "貝田",           color: "#AEB4B8", icon: "img/m11.png?v=3" },
     { id: "m12", name: "大西",           color: "#E23B2E", icon: "img/m12.png?v=3" },
     { id: "m13", name: "相神",           color: "#C9A227", icon: "img/m13.png?v=3" },
-    { id: "m14", name: "中浜",           color: "#EDEDED", icon: "img/m14.png?v=3" }
+    { id: "m14", name: "中浜",           color: "#EDEDED", icon: "img/m14.png?v=3" },
+     { id: "g01", name: "⭐ガチャガチャ⭐", color: "#E53935", icon: "img/g01.png?v=3" }
   ],
 
   /* ⑦ トイレの場所（増やす場合はここに { name, lat, lng } の行を足すだけ） */
